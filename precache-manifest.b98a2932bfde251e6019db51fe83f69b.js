6df75c42da692627c6456a2b27da8ac7de1e00e8
Wayne Cheng <wayne@che.ng>
2019-12-22 00:47:52 -0800

    Updates

---
+++ precache-manifest.b98a2932bfde251e6019db51fe83f69b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e8f1e4b080433b0a5c35c9117ef91c2",
+    "url": "/tft-items/static/media/item-table.4e8f1e4b.png"
+  },
+  {
+    "revision": "80ed51ce71c49e18f5d9",
+    "url": "/tft-items/static/js/runtime~main.80ed51ce.js"
+  },
+  {
+    "revision": "4e7caf36d4de048cddec",
+    "url": "/tft-items/static/js/main.4e7caf36.chunk.js"
+  },
+  {
+    "revision": "35431735e4257b98ce9c",
+    "url": "/tft-items/static/js/1.35431735.chunk.js"
+  },
+  {
+    "revision": "4e7caf36d4de048cddec",
+    "url": "/tft-items/static/css/main.08de03dd.chunk.css"
+  },
+  {
+    "revision": "a0960b39be03d3dce701c2a913de6924",
+    "url": "/tft-items/index.html"
+  }
+];
