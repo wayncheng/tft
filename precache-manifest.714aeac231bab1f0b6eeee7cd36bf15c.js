b1291a738859576d4e4ae6bb13dc5f317b11214b
Wayne Cheng <wayne@che.ng>
2020-01-18 00:33:26 -0800

    Updates

---
+++ precache-manifest.714aeac231bab1f0b6eeee7cd36bf15c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2099939e97e6da5e4c",
+    "url": "/tft/static/js/runtime~main.1d209993.js"
+  },
+  {
+    "revision": "3ab4b5b198844f1abded",
+    "url": "/tft/static/js/main.3ab4b5b1.chunk.js"
+  },
+  {
+    "revision": "95c78618865bbe24091b",
+    "url": "/tft/static/js/1.95c78618.chunk.js"
+  },
+  {
+    "revision": "3ab4b5b198844f1abded",
+    "url": "/tft/static/css/main.2138c652.chunk.css"
+  },
+  {
+    "revision": "35fa9b269d4a11f79845728f7b49c7c4",
+    "url": "/tft/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tft/precache-manifest.8707d2b5881cffd90802905d426503c9.js"
+  "/tft/precache-manifest.714aeac231bab1f0b6eeee7cd36bf15c.js"
 );
 
 workbox.clientsClaim();
