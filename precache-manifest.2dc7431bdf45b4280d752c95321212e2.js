439c66573d497855000984ea758259dc4078bae2
Wayne Cheng <wayne@che.ng>
2020-02-06 08:50:26 -0800

    Updates

---
+++ precache-manifest.2dc7431bdf45b4280d752c95321212e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/tft/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "1d2099939e97e6da5e4c",
+    "url": "/tft/static/js/runtime~main.1d209993.js"
+  },
+  {
+    "revision": "dfd8e4092df20c142c43",
+    "url": "/tft/static/js/main.dfd8e409.chunk.js"
+  },
+  {
+    "revision": "8514cb333c65be4e9c3d",
+    "url": "/tft/static/js/1.8514cb33.chunk.js"
+  },
+  {
+    "revision": "dfd8e4092df20c142c43",
+    "url": "/tft/static/css/main.6fda69f1.chunk.css"
+  },
+  {
+    "revision": "319191f01367229048a9f87b268bc80f",
+    "url": "/tft/index.html"
+  }
+];
