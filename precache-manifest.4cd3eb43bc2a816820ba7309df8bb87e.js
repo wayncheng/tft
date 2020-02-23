14d26981ef41f580fe55ffefd657e194e2533260
Wayne Cheng <wayne@che.ng>
2020-02-22 21:49:50 -0800

    Updates

---
+++ precache-manifest.4cd3eb43bc2a816820ba7309df8bb87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/tft/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "1d2099939e97e6da5e4c",
+    "url": "/tft/static/js/runtime~main.1d209993.js"
+  },
+  {
+    "revision": "889fcd71197a5986026d",
+    "url": "/tft/static/js/main.889fcd71.chunk.js"
+  },
+  {
+    "revision": "3e8029b9c63a688849c5",
+    "url": "/tft/static/js/1.3e8029b9.chunk.js"
+  },
+  {
+    "revision": "889fcd71197a5986026d",
+    "url": "/tft/static/css/main.5b51f36c.chunk.css"
+  },
+  {
+    "revision": "9cf0b8d64f39ae59c50ad3d89c567734",
+    "url": "/tft/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tft/precache-manifest.b7adc53b4b0efee56bc070a10ecac665.js"
+  "/tft/precache-manifest.4cd3eb43bc2a816820ba7309df8bb87e.js"
 );
 
 workbox.clientsClaim();
