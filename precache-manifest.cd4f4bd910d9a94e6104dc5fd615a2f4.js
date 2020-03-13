c25d3d92c83f2b6707e354f6ba9f9cba211e4db1
Wayne Cheng <wayne@che.ng>
2020-03-13 10:24:01 -0700

    Updates

---
+++ precache-manifest.cd4f4bd910d9a94e6104dc5fd615a2f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc91fcf8208f7de32d744bcc8653273e",
+    "url": "/tft/static/media/tft-sheet-item-10.5.cc91fcf8.png"
+  },
+  {
+    "revision": "68efcea5ba10a7028682",
+    "url": "/tft/static/css/main.d4206434.chunk.css"
+  },
+  {
+    "revision": "840dfecdbc34154c8508",
+    "url": "/tft/static/js/1.840dfecd.chunk.js"
+  },
+  {
+    "revision": "1d2099939e97e6da5e4c",
+    "url": "/tft/static/js/runtime~main.1d209993.js"
+  },
+  {
+    "revision": "1f724f2221eb90731c515782cd805475",
+    "url": "/tft/static/media/tft-sheet-item-beta.1f724f22.png"
+  },
+  {
+    "revision": "b7e8a78eddb165ca9eb8d959ca558433",
+    "url": "/tft/static/media/tft-sheet-full-beta.b7e8a78e.png"
+  },
+  {
+    "revision": "b5445d6639a74d14173157e71a9c1508",
+    "url": "/tft/static/media/tft-sheet-wide-beta.b5445d66.png"
+  },
+  {
+    "revision": "68efcea5ba10a7028682",
+    "url": "/tft/static/js/main.68efcea5.chunk.js"
+  },
+  {
+    "revision": "a5370fbaaa37c1064466c48d9b42ef04",
+    "url": "/tft/static/media/tft-sheet-full-10.5.a5370fba.png"
+  },
+  {
+    "revision": "3b468ddafedac02676a4587595dbccfb",
+    "url": "/tft/static/media/tft-sheet-wide-10.5.3b468dda.png"
+  },
+  {
+    "revision": "cc91fcf8208f7de32d744bcc8653273e",
+    "url": "/tft/static/media/tft-sheet-item-10.4.cc91fcf8.png"
+  },
+  {
+    "revision": "a5370fbaaa37c1064466c48d9b42ef04",
+    "url": "/tft/static/media/tft-sheet-full-10.4.a5370fba.png"
+  },
+  {
+    "revision": "3b468ddafedac02676a4587595dbccfb",
+    "url": "/tft/static/media/tft-sheet-wide-10.4.3b468dda.png"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/tft/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "8b7c0dff611d2e9f024f804672342055",
+    "url": "/tft/index.html"
+  }
+];
